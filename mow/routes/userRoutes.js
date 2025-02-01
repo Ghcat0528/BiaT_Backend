806816c092cfff5f9970d15a4c13b4c42e4a95e6
Ghcat0528 <fcatalanog7@gmail.com>
2025-01-31 20:36:21 -0700

    user routes

--- mow/routes/userRoutes.js
+++ mow/routes/userRoutes.js
@@ -16,4 +16,59 @@ router.get('/aboutMe', authenticate, async (req, res) => {
     }
 });
 
+router.get('/users', authenticate, async (req, res) => {
+    try{
+        const users = await prisma.user.findMany({
+            select: { id: true, firstName: true, lastName: true, email: true}
+        });
+        res.json(users)
+    } catch (error){
+        res.status(500).json({ error: 'Internal server error :('})
+    }
+});
+
+router.get('/users/:id', authenticate, async (req, res) => {
+    try{
+        const user = await prisma.user.findUnique({
+            where: { id: req.params.id},
+            select: { id: true, firstName: true, lastName: true, email: true}
+        });
+        if (!user) {
+            return res,status(404).json({ error: 'User not found...'})
+        }
+        res.json(user);
+    } catch(error) {
+        res.status(500).json({ error: 'Internal server error. Sigh'})
+    }
+});
+
+router.delete('/users/:id', authenticate, async (req, res) => {
+    try{ 
+        await prisma.user.delete({ where: { id: req.params.id}});
+        res.json({message: 'You deleted that ho'});
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error. Sigh'})
+    }
+});
+const bcrypt = require('bcryptjs');
+
+router.put('/users/:id', authenticate, async (req, res) => {
+    try{ 
+        const { firstName, lastName, email, password} = req.body;
+        const updateData = { firstName, lastName, email};
+        if (password) {
+            updateData.password = await bcrypt.hash(password, 10);
+        }
+        const updatedUser = await prisma.user.update({
+            where: { id: req.params.id},
+            data: updateData,
+            select: { id: true, firstName: true, lastName: true, email: true}
+        });
+        res.json(updatedUser)
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error. Hmm'})
+    }
+});
+
+
 module.exports = router;
